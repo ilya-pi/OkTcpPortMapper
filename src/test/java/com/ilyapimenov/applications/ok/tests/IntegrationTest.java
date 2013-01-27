827556a9b12c3737a3d6bb549e4d4dd6b15a3dcb
badmotherfucker <ilya.pimenov@gmail.com>
2013-01-27 14:59:46 +0100

    Added integration test through http-commons, can't test right now as internet is dead for whatever the reason

---
+++ src/test/java/com/ilyapimenov/applications/ok/tests/IntegrationTest.java
@@ -0,0 +1,42 @@
+package com.ilyapimenov.applications.ok.tests;
+
+import com.ilyapimenov.applications.ok.TcpPortMapper;
+import com.ilyapimenov.applications.ok.util.ConfParser;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
+import static junit.framework.Assert.assertTrue;
+
+/**
+ * @author Ilya Pimenov
+ *         <p/>
+ *         Tests whether we would be able to access ok.ru through localhost:8080 provided correct configuration
+ */
+public class IntegrationTest {
+
+    private static String confString =
+            "ok.localPort = 8080\n" +
+                    "ok.remoteHost = www.ya.ru\n" +
+                    "rock.remoteHost = www.odnoklassniki.ru\n" +
+                    "ok.remotePort = 80\n";
+
+    @Before
+    public void setup() throws Exception {
+        new TcpPortMapper().start(ConfParser.parse(new ByteArrayInputStream(confString.getBytes())));
+    }
+
+    @Test
+    public void testOk() throws IOException {
+        HttpClient httpClient = new HttpClient();
+        GetMethod getMethod = new GetMethod("http://localhost:8080");
+        httpClient.executeMethod(getMethod);
+        //todo refine, check for a better trigger once internet is back again
+        assertTrue(getMethod.getResponseBodyAsString().contains("ok.ru"));
+    }
+
+}
